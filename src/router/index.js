5c071f1e06ef33a3d7578d1cac1b2b64111b99d8
okxooxoo <okxooxoo@naver.com>
2024-11-19 11:17:19 +0900

    feat: create CalendarView

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import IntroView from '@/views/IntroView.vue'
 import HomeView from '@/views/HomeView.vue'
 import RecordView from '@/views/RecordView.vue'
+import CalendarView from '@/views/CalendarView.vue'
 import ProfileView from '@/views/ProfileView.vue'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegistView from '@/views/auth/RegistView.vue'
@@ -25,6 +26,12 @@ const router = createRouter({
       path: "/record",
       name: "record",
       component: RecordView,
+    },
+
+    {
+      path: '/calendar',
+      name: 'calendar',
+      component: CalendarView,
     },
 
     {
---
+++ src/views/CalendarView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <RangeCalendar />
+  </div>
+</template>
+
+<script setup>
+import RangeCalendar from '@/components/ui/range-calendar/RangeCalendar.vue'
+</script>
