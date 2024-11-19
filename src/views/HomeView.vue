87c86e3a2f97e6ceac12bcb3f5e69587ea638de9
okxooxoo <okxooxoo@naver.com>
2024-11-19 10:56:45 +0900

    feat: 홈 화면에 비디오 박스 추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,21 @@
 <script setup>
 import KakaoMap from '@/components/KakaoMap.vue'
+import VideoBox from '@/components/VideoBox.vue'
 </script>
 
 <template>
-  <KakaoMap />
+  <div class="home-view">
+    <KakaoMap />
+    <VideoBox />
+  </div>
 </template>
+
+<style scoped>
+.home-view {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: calc(100vh - 4rem);
+}
+</style>
