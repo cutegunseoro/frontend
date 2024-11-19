fe320a3c1eb933d087c7172d2658e3eafa00c51c
okxooxoo <okxooxoo@naver.com>
2024-11-19 11:09:12 +0900

    chore: shadcn-vue 설정

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,6 +18,9 @@ export default defineConfig({
     },
   },
   css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
     preprocessorOptions: {
       scss: {
         additionalData: `@use "@/assets/colors/colors";`,
